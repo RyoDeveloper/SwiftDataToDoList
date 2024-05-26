2e57b0eb2f4a64c29ffde0db5d377fcb24028327
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2024-05-26 22:50:01 +0900

    Initial Commit

---
+++ SwiftDataToDoList/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftDataToDoList
+//  
+//  https://github.com/RyoDeveloper/SwiftDataToDoList
+//  Copyright © 2024 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataToDoList/SwiftDataToDoListApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftDataToDoListApp.swift
+//  SwiftDataToDoList
+//  
+//  https://github.com/RyoDeveloper/SwiftDataToDoList
+//  Copyright © 2024 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
